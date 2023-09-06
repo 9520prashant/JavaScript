36d0cd664da2e648e5166a7a4b4a95762ca06b78
Prashant varma <prashantvarma5083@gmail.com>
2023-09-06 23:34:03 +0530

    add someting

--- DailyGoalProject/index.js
+++ DailyGoalProject/index.js
@@ -1,2 +1,12 @@
-const title = document.getElementById("title")
+const title = document.getElementById("title");
 const description = document.getElementById("description");
+const container = document.querySelector(".container");
+
+const div = document.createElement('div');
+div.setAttribute("class", "task");
+
+div.innerText = "Hi, I am Task DIv";
+
+const addTodos = ()=>{
+    container.append(div);
+}
