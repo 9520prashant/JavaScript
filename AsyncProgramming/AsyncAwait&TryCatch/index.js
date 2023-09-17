10dc3d944309700c7af07f0d9c4468fb79d5d712
Prashant varma <prashantvarma5083@gmail.com>
2023-09-17 22:42:24 +0530

    TryCatch Done

--- AsyncProgramming/AsyncAwait&TryCatch/index.js
+++ AsyncProgramming/AsyncAwait&TryCatch/index.js
@@ -2,7 +2,20 @@ const url = "https://catfact.ninja/fact";
 const h1 = document.querySelector("h1")
 
 const fetchData = async ()=>{
-    const response = await fetch(url);
-    console.log(response);
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error)
+    }   
 }
-fetchData()
+fetchData();
+
+// console.log("a")
+// setTimeout(() => {
+//     console.log("Timeout")
+// }, 0);
+// Promise.resolve().then(()=> console.log("Promise"))
+// console.log("z")
